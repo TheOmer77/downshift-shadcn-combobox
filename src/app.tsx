6fcb626758c5ad2bacca204fb159256ab1645763
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-18 18:06:31 +0200

    Update description in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,11 @@ const App = () => (
   <div className='mx-auto w-full max-w-md space-y-4 px-8 py-16 md:py-32'>
     <div className='space-y-1.5'>
       <h1 className='text-3xl font-bold tracking-tight'>Shadcn/ui Combobox</h1>
-      <p className='text-sm text-muted-foreground'>
-        Autocomplete input and command palette with a list of suggestions.
+      <p className='text-sm text-muted-foreground [&_a]:font-medium [&_a]:text-foreground [&_a]:underline [&_a]:underline-offset-4'>
+        An autocomplete input and command palette with a list of suggestions.
+        Built with <a href='https://ui.shadcn.com/'>shadcn/ui</a> and{' '}
+        <a href='https://www.downshift-js.com/'>Downshift</a>&apos;s useCombobox
+        hook.
       </p>
     </div>
 
